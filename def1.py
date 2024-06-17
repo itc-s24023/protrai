cd5b25252922c4dff90a87de239d8a1e655c5957
s24023 <s24023@std.it-college.ac.jp>
2024-06-17 14:23:59 +0900

    add def1.py

--- def1.py
+++ def1.py
@@ -1,2 +1,9 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# 24023
+# 関数で「こんにちは」を3回表示するプログラム
+
+def sayhello():
+    print("こんにちわ")
+
+sayhello()
+sayhello()
+sayhello()
