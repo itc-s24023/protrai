620cafd3604cebf2a9771a32c149804f36352ee8
s24023 <s24023@std.it-college.ac.jp>
2024-06-17 15:48:37 +0900

    add def4_kadai.py

---
+++ def4_kadai.py
@@ -0,0 +1,18 @@
+# 24023
+# メインの処理をmain()関数に行わせるおみくじプログラム
+
+
+
+import random
+def omikuji():
+    kuji = ["大吉","中吉","小吉","凶"]
+    return random.choice(kuji)
+
+
+# エントリーポイントの定義
+def main():
+    kekka = omikuji()
+    print("結果は",kekka,"です")
+
+if __name__== "__main__":
+    main()
